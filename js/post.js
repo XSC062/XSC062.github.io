a570ccb658c9d57f101b2c8d2888d2bef1df8517
XSC062 <xsc062@qq.com>
2023-01-29 15:46:02 +0800

    Site updated: 2023-01-29 15:46:01

---
+++ js/post.js
@@ -0,0 +1,14 @@
+function setHitokoto() {
+    $.get('https://v1.hitokoto.cn/', function (data) {
+        if (typeof data === 'string')
+            data = JSON.parse(data);
+        $('#hitokoto-loader').removeClass('active');
+        $('#hitokoto-content').css('display', '').text(data.hitokoto);
+        if (data.from) {
+            if (data.from_who)
+                $('#hitokoto-from').css('display', '').text('来自「' + data.from + '」：' + data.from_who);
+           else
+                $('#hitokoto-from').css('display', '').text('来自「' + data.from + '」');
+        }
+    });
+}
