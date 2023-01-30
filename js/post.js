78b585f90eb7be42c28da2e99d2ce747b455c694
XSC062 <xsc062@qq.com>
2023-01-30 21:31:23 +0800

    Site updated: 2023-01-30 21:31:22

--- js/post.js
+++ js/post.js
@@ -1,3 +1,5 @@
+const { page } = require("hexo/lib/plugins/helper/is");
+
 function setHitokoto() {
     $.get('https://v1.hitokoto.cn/', function (data) {
         if (typeof data === 'string')
@@ -11,4 +13,33 @@ function setHitokoto() {
                 $('#hitokoto-from').css('display', '').text('来自「' + data.from + '」');
         }
     });
+}
+
+function setOrder() {
+    var revPosts = site.posts.data;
+    revPosts.sort(function (a, b) { return b.date - a.date; });
+    return revPosts;
+}
+
+function getPrevPost() {
+    var result = setOrder();
+    var last = 0;
+    result.forEach(function (post) {
+        if (post.source == page.source)
+            return last;
+        last = post;
+    });
+    return 0;
+}
+
+function getNextPost() {
+    var result = setOrder();
+    var flag = false;
+    result.forEach(function (post) {
+        if (post.source == page.source)
+            flag = true;
+        if (flag == true)
+            return post;
+    });
+    return 0;
 }
