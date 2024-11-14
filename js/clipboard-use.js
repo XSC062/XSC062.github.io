0e009a65837327e34b546e1032d31566529f2f7c
XSC062 <xsc062@outlook.com>
2024-11-14 22:17:32 +0800

    Site updated: 2024-11-14 22:17:30

---
+++ js/clipboard-use.js
@@ -0,0 +1,54 @@
+/*页面载入完成后，创建复制按钮*/
+!function (e, t, a) {
+    /* code */
+    var initCopyCode = function() {
+        var copyHtml = '';
+        copyHtml += '<button type="button" class="btn-copy" data-clipboard-snippet="">';
+        copyHtml += '  <i class="far fa-clone"></i><span>Copy</span>';
+        copyHtml += '</button>';
+        $(".line-numbers").wrap($('<div class="code-wrap"></div>'));
+        $('.code-wrap').prepend(copyHtml);
+        var clipboard = new ClipboardJS('.btn-copy', {
+            target: function(trigger) {
+                return trigger.nextElementSibling;
+            }
+        });
+        // 成功提示
+        clipboard.on('success', function(e) {
+            e.trigger.style = "opacity: 1;";
+            e.trigger.getElementsByTagName('i')[0].style = "display: none;";
+            e.trigger.getElementsByTagName('span')[0].innerHTML = "Copied!"
+            e.clearSelection();
+            var displayKeeper = setInterval(() => {
+                e.trigger.style = "opacity: 1;";
+            }, 10);
+            setTimeout(() => {
+                e.trigger.getElementsByTagName('i')[0].style = null;
+                e.trigger.getElementsByTagName('span')[0].innerHTML = "Copy"
+                e.trigger.style = null;
+            }, 700);
+            setTimeout(() => {
+                clearInterval(displayKeeper);
+                e.trigger.style = null;
+            }, 1000);
+        });
+        clipboard.on('error', function(e) {
+            e.trigger.getElementsByTagName('i')[0].style = "display: none;";
+            e.trigger.getElementsByTagName('span')[0].innerHTML = "Something\'s wrong. Maybe try again?"
+            e.clearSelection();
+            var displayKeeper = setInterval(() => {
+                e.trigger.style = "opacity: 1;";
+            }, 10);
+            setTimeout(() => {
+                e.trigger.getElementsByTagName('i')[0].style = null;
+                e.trigger.getElementsByTagName('span')[0].innerHTML = "Copy"
+                e.trigger.style = null;
+            }, 700);
+            setTimeout(() => {
+                clearInterval(displayKeeper);
+                e.trigger.style = null;
+            }, 1000);
+        });
+    }
+    initCopyCode();
+}(window, document);
