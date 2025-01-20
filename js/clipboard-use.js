16ce8c1e8504e5ec3542aba882449088bc4adaed
XSC062 <xsc062@outlook.com>
2025-01-20 21:20:53 +0800

    Site updated: 2025-01-20 21:20:52

--- js/clipboard-use.js
+++ js/clipboard-use.js
@@ -1,10 +1,9 @@
 /*页面载入完成后，创建复制按钮*/
 !function (e, t, a) {
-    /* code */
     var initCopyCode = function() {
         var copyHtml = '';
         copyHtml += '<button type="button" class="btn-copy" data-clipboard-snippet="">';
-        copyHtml += '  <i class="far fa-clone"></i><span>Copy</span>';
+        copyHtml += '<i class="far fa-clone"></i><span>Copy</span>';
         copyHtml += '</button>';
         $(".line-numbers").wrap($('<div class="code-wrap"></div>'));
         $('.code-wrap').prepend(copyHtml);
@@ -18,12 +17,14 @@
             e.trigger.style = "opacity: 1;";
             e.trigger.getElementsByTagName('i')[0].style = "display: none;";
             e.trigger.getElementsByTagName('span')[0].innerHTML = "Copied!"
+            e.trigger.getElementsByTagName('span')[0].style = "margin-left: 0;"
             e.clearSelection();
             var displayKeeper = setInterval(() => {
                 e.trigger.style = "opacity: 1;";
             }, 10);
             setTimeout(() => {
                 e.trigger.getElementsByTagName('i')[0].style = null;
+                e.trigger.getElementsByTagName('span')[0].style = null
                 e.trigger.getElementsByTagName('span')[0].innerHTML = "Copy"
                 e.trigger.style = null;
             }, 700);
@@ -35,12 +36,14 @@
         clipboard.on('error', function(e) {
             e.trigger.getElementsByTagName('i')[0].style = "display: none;";
             e.trigger.getElementsByTagName('span')[0].innerHTML = "Something\'s wrong. Maybe try again?"
+            e.trigger.getElementsByTagName('span')[0].style = "margin-left: 0;"
             e.clearSelection();
             var displayKeeper = setInterval(() => {
                 e.trigger.style = "opacity: 1;";
             }, 10);
             setTimeout(() => {
                 e.trigger.getElementsByTagName('i')[0].style = null;
+                e.trigger.getElementsByTagName('span')[0].style = null
                 e.trigger.getElementsByTagName('span')[0].innerHTML = "Copy"
                 e.trigger.style = null;
             }, 700);
